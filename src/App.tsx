e6139064c48940729825ea9eb2d7fe09e789a2f5
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-13 14:57:42 +0400

    feat: close menu after click out the menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header, Routing } from "./layouts";
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div className="w-full min-h-screen">
       <Header showMenu={showMenu} setShowMenu={setShowMenu} />
--- src/components/shared/MobileMenu.tsx
+++ src/components/shared/MobileMenu.tsx
@@ -1,39 +1,53 @@
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 type PropsType = {
   setShowMenu: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const MobileMenu: React.FC<PropsType> = ({ setShowMenu }) => {
-  return (
-    <div className="w-64 h-[calc(100vh-64px)] absolute pb-6 px-8 top-16 left-0 bg-gray flex flex-col">
-      <ul className="list-none mt-16 flex flex-col gap-6">
-        <Link
-          to="/about"
-          className="mono font-bold text-lg text-light-gray"
-          onClick={() => setShowMenu(false)}
-        >
-          About
-        </Link>
-        <Link
-          to="/locations"
-          className="mono font-bold text-lg text-light-gray"
-          onClick={() => setShowMenu(false)}
-        >
-          Location
-        </Link>
-        <Link
-          to="/careers"
-          className="mono font-bold text-lg text-light-gray"
-          onClick={() => setShowMenu(false)}
-        >
-          Careers
-        </Link>
-      </ul>
+  const page = useRef<HTMLDivElement | null>(null);
+  const closeMenu: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    if (page.current === event.target) {
+      setShowMenu(false);
+    }
+  };
 
-      <button className="w-full py-3 bg-yellow mono font-bold text-base text-white border-none mt-auto">
-        Get Scootin
-      </button>
+  return (
+    <div
+      className="h-[calc(100vh-64px)] w-full absolute top-16 left-0 bg-black bg-opacity-70"
+      ref={page}
+      onClick={closeMenu}
+    >
+      <div className="w-64 h-full pb-6 px-8 bg-gray flex flex-col">
+        <ul className="list-none mt-16 flex flex-col gap-6">
+          <Link
+            to="/about"
+            className="mono font-bold text-lg text-light-gray"
+            onClick={() => setShowMenu(false)}
+          >
+            About
+          </Link>
+          <Link
+            to="/locations"
+            className="mono font-bold text-lg text-light-gray"
+            onClick={() => setShowMenu(false)}
+          >
+            Location
+          </Link>
+          <Link
+            to="/careers"
+            className="mono font-bold text-lg text-light-gray"
+            onClick={() => setShowMenu(false)}
+          >
+            Careers
+          </Link>
+        </ul>
+
+        <button className="w-full py-3 bg-yellow mono font-bold text-base text-white border-none mt-auto">
+          Get Scootin
+        </button>
+      </div>
     </div>
   );
 };
