3c50aa1c14ceee0d410f5d3e4fbc9d85df232b23
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-14 11:13:03 +0400

    work width info cards for about page

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,3 +1,6 @@
+import { BetterLiving, DigitalEra } from "../../assets";
+import { LeftUpwardArrow, RightArrow } from "../../svg";
+
 const About = () => {
   return (
     <div className="w-full">
@@ -6,6 +9,48 @@ const About = () => {
           About
         </h2>
       </div>
+      <ul className="mt-[72px] flex flex-col gap-[120px] w-full px-8">
+        <div className="w-full relative">
+          <div className="absolute top-28 -right-[420px]">
+            <LeftUpwardArrow />
+          </div>
+          <img
+            src={DigitalEra}
+            alt="telemetry"
+            className="rounded-full mb-14"
+          />
+          <div className="w-full flex flex-col gap-8">
+            <h3 className="mono text-semi-gray text-[32px] tracking-[-1.429px] font-bold text-center">
+              Mobility for the digital era
+            </h3>
+            <p className="lexend font-normal text-[15px] text-opacity-gray text-center">
+              Getting around should be simple (and even fun!) for everyone. We
+              embrace technology to provide low cost, smart access to scooters
+              at your fingertips.
+            </p>
+          </div>
+        </div>
+        <div className="w-full relative">
+          <div className="absolute top-0 -left-[150px]">
+            <RightArrow />
+          </div>
+          <img
+            src={BetterLiving}
+            alt="telemetry"
+            className="rounded-full mb-14"
+          />
+          <div className="w-full flex flex-col gap-8">
+            <h3 className="mono text-semi-gray text-[32px] tracking-[-1.429px] font-bold text-center">
+              Better urban living
+            </h3>
+            <p className="lexend font-normal text-[15px] text-opacity-gray text-center">
+              We’re helping connect cities and bring people closer together. Our
+              scooters are also fully-electric and we offset the minimal carbon
+              footprint for each ride.
+            </p>
+          </div>
+        </div>
+      </ul>
     </div>
   );
 };
