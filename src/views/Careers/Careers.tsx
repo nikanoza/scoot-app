21e59bdb31c985861498249841cd35b3dc5dad9e
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-14 15:56:08 +0400

    feat: work with careers page baner

--- src/views/Careers/Careers.tsx
+++ src/views/Careers/Careers.tsx
@@ -1,5 +1,13 @@
 const Careers = () => {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <div className="w-full py-[60px] careers-hero-mobile">
+        <h2 className="mono font-bold text-white text-[40px] text-center">
+          Careers
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Careers;
